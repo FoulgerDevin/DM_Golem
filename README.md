b40f50b275aec3e8b2fe24b4028b9bb4e03848a0
foulgerdevin <foulgerdevin@gmail.com>
2018-02-02 19:25:05 -0800

    Version 0.0.2 - can roll now

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # DM_Golem
 
-This is a simple bot that will roll dice at first, but maybe some day it will do more.
+This is a simple bot that will roll dice at first, but maybe some day it will do more. Runs with NodeJS.
 
-Run this commaned `npm install discord.io winston --save` to install the packages you need. 
+Run this command `npm install discord.io winston --save` to install the packages you need. 
 
 If you run into a problem where the bot runs and nothing happens, you might be pointed at a discord gateway that is discontinued. In order to get around that, I ran this instead `npm install woor/discord.io#gateway_v6 winston --save`. 
--- js/bot.js
+++ js/bot.js
@@ -1,7 +1,14 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('../json/auth.json');
+var idk = "I don't know what you're trying to do, but use $help to see what I am capable of.";
+var helpMessage = "```Commands\n" 
+				+ "$help          | Displays a list of commands.\n" 
+				+ "$echo [string] | Will message back what ever string you provided.\n" 
+				+ "$roll [XdY]    | Roll a dice where X is the number of times you want to roll the dice " 
+				+ "and Y is the number of sides the dice has.```";
 
+				
 //configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -27,7 +34,7 @@ bot.on('ready', function(evnt) {
 
 bot.on('message', function(user, userId, channelId, message, evnt) {
 	//Listen for messages that start with $
-	if (message.substring(0,1) == '$') {
+	if (message.substring(0,1) == '$' && ((channelId == "406231410047975424") || (channelId == "345347162420281357"))) {
 		var args = message.substring(1).split(' ');
 		var cmd = args[0];
 		
@@ -44,12 +51,95 @@ bot.on('message', function(user, userId, channelId, message, evnt) {
 				});
 			break;
 			
-			case 'echo': 
+			case 'help':
 				bot.sendMessage({
 					to: channelId,
-					message: args
+					message: helpMessage
 				});
 			break;
+			
+			case 'echo': 
+				var message = args[0];
+				if (args.length > 1) {
+					for (i = 1; i < args.length; i++) {
+						message += ' ' + args[i];
+					}
+				}
+				bot.sendMessage({
+					to: channelId,
+					message: message
+				});
+			break;
+			
+			case 'roll': 
+				var incomplete  = "The command passed was incorrect. Needs to be $roll [XdY].";
+				var rollMessage = "Please pick a integer less than 21";
+				var diceMessage = "Please choose either a 4, 6, 8, 10, 12, or 20 sided die.";
+				var diceArgs = args[0].substring(0).split('d');
+				
+				//Check to see if the args are even correct
+				if (diceArgs.length == 2) {
+					//Check if the provided number of rolls is good
+					if ((diceArgs[0] == parseInt(diceArgs[0], 10)) && (diceArgs[0] < 21)) {
+						var numberOfRolls = diceArgs[0];
+					} else {
+						bot.sendMessage({
+							to: channelId,
+							message: rollMessage
+						});
+						break;
+					}
+					
+					//Check if the number of sides is good
+					if (diceArgs[1] == parseInt(diceArgs[1], 10)) {
+						//var numberOfSides = diceArgs[1];
+						if ((diceArgs[1] == 4) || (diceArgs[1] == 6) || (diceArgs[1] == 8) || (diceArgs[1] == 10) || 
+							(diceArgs[1] == 12) || (diceArgs[1] == 20)) {
+								
+							var numberOfSides = diceArgs[1];
+						} else {
+							bot.sendMessage({
+								to: channelId,
+								message: diceMessage
+							});
+							break;
+						}
+					} else {
+						bot.sendMessage({
+							to: channelId,
+							message: diceMessage
+						});
+						break;
+					}
+					
+					//Message to return
+					var result = "```";
+					
+					//Do the actual rolling now
+					for (i = 0; i < numberOfRolls; i++) {
+						result += "Roll " + (i+1) + ": " + Math.floor((Math.random() * numberOfSides) + 1) + "\n";
+					}
+					
+					result += "```";
+					
+					bot.sendMessage({
+						to: channelId,
+						message: result
+					})
+				} else {
+					bot.sendMessage({
+						to: channelId,
+						message: incomplete
+					});
+					break;
+				}
+			break;
+			
+			default:
+				bot.sendMessage({
+					to: channelId,
+					message: idk
+				});
 		}
 	}
 });
